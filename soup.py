f4bad48e691f5c6cf3906fc71f071b0eef2a03aa
Santino Quiroga <107203507+SantinoQuiroga23@users.noreply.github.com>
2023-10-26 02:15:33 -0300

    Update soup.py

--- soup.py
+++ soup.py
@@ -1,28 +1,14 @@
 import requests
 from icecream import ic
 
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
-
 dolar_oficial: str = "https://dolarapi.com/v1/dolares/oficial"
 dolar_blue: str = "https://dolarapi.com/v1/dolares/blue"
 dolar_bolsa: str = "https://dolarapi.com/v1/dolares/bolsa"
 crypto_price: str = 'https://api.coingecko.com/api/v3/simple/price'
 
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
 lista: list = [dolar_blue, dolar_oficial, dolar_bolsa, crypto_price]
 
 ids: str = "usd-coin"
-
-#CUMPLE CON ESTANDARES PEP 8
 
 def crypto(url: str, ids: str):
     params = {'ids': ids, 'vs_currencies': 'ars'}
@@ -33,15 +19,6 @@ def crypto(url: str, ids: str):
     else:
         ic("error: crypto currency its not avaible")
     return data
-
-
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
 
 def dolar(url: str):
     response = requests.get(url=url)
@@ -57,19 +34,3 @@ if __name__ == '__main__':
     dolar(url=dolar_oficial)
     crypto(url=crypto_price, ids=ids)
 
-
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
-#CUMPLE CON ESTANDARES PEP 8
